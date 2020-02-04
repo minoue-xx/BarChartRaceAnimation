9e4923b7ea7ccbc4d90c07cc256b3992a1a7ce81
Michio Inoue <790354+minoue-xx@users.noreply.github.com>
2020-02-04 12:36:05 +0900

    Update importData.m

--- example/RegionalPopulationJapan/importData.m
+++ example/RegionalPopulationJapan/importData.m
@@ -1,33 +1,33 @@
-%% ƒXƒvƒŒƒbƒhƒV[ƒg‚©‚çƒf[ƒ^‚ğƒCƒ“ƒ|[ƒg
-% Ÿ‚ÌƒXƒvƒŒƒbƒhƒV[ƒg‚©‚çƒf[ƒ^‚ğƒCƒ“ƒ|[ƒg‚·‚éƒXƒNƒŠƒvƒg:
+%% ã‚¹ãƒ—ãƒ¬ãƒƒãƒ‰ã‚·ãƒ¼ãƒˆã‹ã‚‰ãƒ‡ãƒ¼ã‚¿ã‚’ã‚¤ãƒ³ãƒãƒ¼ãƒˆ
+% æ¬¡ã®ã‚¹ãƒ—ãƒ¬ãƒƒãƒ‰ã‚·ãƒ¼ãƒˆã‹ã‚‰ãƒ‡ãƒ¼ã‚¿ã‚’ã‚¤ãƒ³ãƒãƒ¼ãƒˆã™ã‚‹ã‚¹ã‚¯ãƒªãƒ—ãƒˆ:
 %
-%    ƒ[ƒNƒuƒbƒN: C:\Users\minoue\gitlab\work_BarChart\05k5-5.xlsx
-%    ƒ[ƒNƒV[ƒg: ‘æ5•\
+%    ãƒ¯ãƒ¼ã‚¯ãƒ–ãƒƒã‚¯: 05k5-5.xlsx
+%    ãƒ¯ãƒ¼ã‚¯ã‚·ãƒ¼ãƒˆ: ç¬¬5è¡¨
 %
-% MATLAB ‚©‚ç‚Ì©“®¶¬“ú: 2020/01/29 06:38:14
+% MATLAB ã‹ã‚‰ã®è‡ªå‹•ç”Ÿæˆæ—¥: 2020/01/29 06:38:14
 % Copyright (c) 2020 Michio Inoue
-%% ƒCƒ“ƒ|[ƒg ƒIƒvƒVƒ‡ƒ“‚Ìİ’è‚¨‚æ‚Ñƒf[ƒ^‚ÌƒCƒ“ƒ|[ƒg
+%% ã‚¤ãƒ³ãƒãƒ¼ãƒˆ ã‚ªãƒ—ã‚·ãƒ§ãƒ³ã®è¨­å®šãŠã‚ˆã³ãƒ‡ãƒ¼ã‚¿ã®ã‚¤ãƒ³ãƒãƒ¼ãƒˆ
 opts = spreadsheetImportOptions("NumVariables", 83);
 
-% ƒV[ƒg‚Æ”ÍˆÍ‚Ìw’è
-opts.Sheet = "‘æ5•\";
+% ã‚·ãƒ¼ãƒˆã¨ç¯„å›²ã®æŒ‡å®š
+opts.Sheet = "ç¬¬5è¡¨";
 opts.DataRange = "C8:CG57";
 
-% —ñ–¼‚ÆŒ^‚Ìw’è
+% åˆ—åã¨å‹ã®æŒ‡å®š
 opts.VariableNames = ["VarName3", "VarName4", "VarName5", "VarName6", "VarName7", "VarName8", "VarName9", "VarName10", "VarName11", "VarName12", "VarName13", "VarName14", "VarName15", "VarName16", "VarName17", "VarName18", "VarName19", "VarName20", "VarName21", "VarName22", "VarName23", "VarName24", "VarName25", "VarName26", "VarName27", "VarName28", "VarName29", "VarName30", "VarName31", "VarName32", "VarName33", "VarName34", "VarName35", "VarName36", "VarName37", "VarName38", "VarName39", "VarName40", "VarName41", "VarName42", "VarName43", "VarName44", "VarName45", "VarName46", "VarName47", "VarName48", "VarName49", "VarName50", "VarName51", "VarName52", "VarName53", "VarName54", "VarName55", "VarName56", "VarName57", "VarName58", "VarName59", "VarName60", "VarName61", "VarName62", "VarName63", "VarName64", "VarName65", "VarName66", "VarName67", "VarName68", "VarName69", "VarName70", "VarName71", "VarName72", "VarName73", "VarName74", "VarName75", "VarName76", "VarName77", "VarName78", "VarName79", "VarName80", "VarName81", "VarName82", "VarName83", "VarName84", "Thousandpersons"];
 opts.VariableTypes = ["string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string", "string"];
 
-% •Ï”ƒvƒƒpƒeƒB‚ğw’è
+% å¤‰æ•°ãƒ—ãƒ­ãƒ‘ãƒ†ã‚£ã‚’æŒ‡å®š
 opts = setvaropts(opts, ["VarName3", "VarName4", "VarName5", "VarName6", "VarName7", "VarName8", "VarName9", "VarName10", "VarName11", "VarName12", "VarName13", "VarName14", "VarName15", "VarName16", "VarName17", "VarName18", "VarName19", "VarName20", "VarName21", "VarName22", "VarName23", "VarName24", "VarName25", "VarName26", "VarName27", "VarName28", "VarName29", "VarName30", "VarName31", "VarName32", "VarName33", "VarName34", "VarName35", "VarName36", "VarName37", "VarName38", "VarName39", "VarName40", "VarName41", "VarName42", "VarName43", "VarName44", "VarName45", "VarName46", "VarName47", "VarName48", "VarName49", "VarName50", "VarName51", "VarName52", "VarName53", "VarName54", "VarName55", "VarName56", "VarName57", "VarName58", "VarName59", "VarName60", "VarName61", "VarName62", "VarName63", "VarName64", "VarName65", "VarName66", "VarName67", "VarName68", "VarName69", "VarName70", "VarName71", "VarName72", "VarName73", "VarName74", "VarName75", "VarName76", "VarName77", "VarName78", "VarName79", "VarName80", "VarName81", "VarName82", "VarName83", "VarName84", "Thousandpersons"], "WhitespaceRule", "preserve");
 opts = setvaropts(opts, ["VarName3", "VarName4", "VarName5", "VarName6", "VarName7", "VarName8", "VarName9", "VarName10", "VarName11", "VarName12", "VarName13", "VarName14", "VarName15", "VarName16", "VarName17", "VarName18", "VarName19", "VarName20", "VarName21", "VarName22", "VarName23", "VarName24", "VarName25", "VarName26", "VarName27", "VarName28", "VarName29", "VarName30", "VarName31", "VarName32", "VarName33", "VarName34", "VarName35", "VarName36", "VarName37", "VarName38", "VarName39", "VarName40", "VarName41", "VarName42", "VarName43", "VarName44", "VarName45", "VarName46", "VarName47", "VarName48", "VarName49", "VarName50", "VarName51", "VarName52", "VarName53", "VarName54", "VarName55", "VarName56", "VarName57", "VarName58", "VarName59", "VarName60", "VarName61", "VarName62", "VarName63", "VarName64", "VarName65", "VarName66", "VarName67", "VarName68", "VarName69", "VarName70", "VarName71", "VarName72", "VarName73", "VarName74", "VarName75", "VarName76", "VarName77", "VarName78", "VarName79", "VarName80", "VarName81", "VarName82", "VarName83", "VarName84", "Thousandpersons"], "EmptyFieldRule", "auto");
 
-% ƒf[ƒ^‚ÌƒCƒ“ƒ|[ƒg
-k55 = readtable("C:\Users\minoue\gitlab\work_BarChart\05k5-5.xlsx", opts, "UseExcel", false);
+% ãƒ‡ãƒ¼ã‚¿ã®ã‚¤ãƒ³ãƒãƒ¼ãƒˆ
+k55 = readtable("05k5-5.xlsx", opts, "UseExcel", false);
 
-%% o—ÍŒ^‚Ö‚Ì•ÏŠ·
+%% å‡ºåŠ›å‹ã¸ã®å¤‰æ›
 k55 = table2cell(k55);
 numIdx = cellfun(@(x) ~isnan(str2double(x)), k55);
 k55(numIdx) = cellfun(@(x) {str2double(x)}, k55(numIdx));
 
-%% ˆê•Ï”‚ÌƒNƒŠƒA
+%% ä¸€æ™‚å¤‰æ•°ã®ã‚¯ãƒªã‚¢
 clear opts
